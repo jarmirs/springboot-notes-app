d2fe0cea58ee71c960df00125cef170ae3959187
Jarmir Smith <55027346+jarmirs@users.noreply.github.com>
2022-10-02 23:28:30 -0400

    1st comm

---
+++ src/main/java/com/tts/notes/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.tts.notes.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	@GetMapping("/register")
+	public String register() {
+		return "register";
+	}
+
+}
